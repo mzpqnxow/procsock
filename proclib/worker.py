9337a8198ab06a9e6140a49ded30d923caaa74ca
Adam Greene <github@mzpqnxow.com>
2017-08-30 10:31:24 -0400

    Rename

--- proclib/worker.py
+++ proclib/worker.py
@@ -1,35 +1,45 @@
+# -*- coding: utf-8 -*-
+""" Worker class used to split up work for ps and lsof processing
+
+Miscellaneous functionality used by procksock and/or proclib
+
+Copyright (C) 2017 copyright /at/ mzpqnxow.com under the MIT license
+Please see COPYRIGHT for terms
+"""
+from __future__ import print_function
 from multiprocessing import Process, Queue
-from os import sysconf
+from os import sysconf, sep as SLASH
 import re
-import os
 import sys
 import time
 from glob import glob
 
 
 class ProcToolWorker(object):
+    """ Worker for processing lsof and ps data """
     IP_DATE_RE_COMPILED = re.compile(
         r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\.(\d{12})')
 
     def __init__(self, results_path, cmd, extension='.ret.complete'):
         self._results_path = results_path
         self._debug = None
+        self._run_time = None
         self._cmd = cmd
         self._extension = extension
 
     def _enumerate_acquisitions(self, directory, suffix):
         records = []
         glob_pattern = "*%s" % suffix
-        full_glob = os.sep.join((directory, glob_pattern))
+        full_glob = SLASH.join((directory, glob_pattern))
         for filename in glob(full_glob):
             match = self.IP_DATE_RE_COMPILED.search(filename)
             if not match:
                 raw_input("Malformed file '%s' - continue?" % filename)
-            ip = match.group(1)
-            records.append((self._basename(filename), ip))
+            ip_addr = match.group(1)
+            records.append((self._basename(filename), ip_addr))
         return records
 
-    def _basename(self, name, depth=2):
+    def _basename(self, name):
         return '.'.join(name.split('.')[0:-2])
 
     def _cpu_chunk(self, items, cpu_count):
@@ -44,6 +54,9 @@ class ProcToolWorker(object):
 
     def get_time(self):
         return self._run_time
+
+    def load(self, completed, queue):
+        raise NotImplementedError('implement this in descendant class')
 
     def go_paralell(self, force_process_count=None):
         """
@@ -68,44 +81,44 @@ class ProcToolWorker(object):
         if not force_process_count:
             cpu_count = sysconf('SC_NPROCESSORS_ONLN')
         else:
-            assert type(force_process_count) == int
+            assert isinstance(force_process_count, int)
             cpu_count = force_process_count
 
         completed_sets = self._cpu_chunk(completed, cpu_count)
         print('- %d total IP process listings discovered on filesystem' % (
             len(completed)))
         print('- sysconf(SC_NPROCESSORS_ONLN) == %d' % cpu_count)
-        print
+        print()
         print('Work will be split across %d %s' % (
             cpu_count,
             'logical processors' if not force_process_count else 'processes'))
-        print
+        print()
         processes = []
         print('Starting processes...')
         for i in completed_sets:
             queue = Queue()
             process = Process(target=self.load, args=(i, queue))
             processes.append((process, queue))
-        for cpu, (p, q) in enumerate(processes):
+        for cpu, (process, queue) in enumerate(processes):
             print('(%s) start %s #%d' % (
                 self._cmd,
                 'CPU' if not force_process_count else 'process', cpu))
-            p.start()
-        print
+            process.start()
+        print()
         results = {}
-        for cpu, (p, q) in enumerate(processes):
+        for cpu, (process, queue) in enumerate(processes):
             sys.stdout.write('-- waiting for result from %s#%.2d ... ' % (
                 'CPU' if not force_process_count else 'process', cpu))
             sys.stdout.flush()
-            result = q.get()
+            result = queue.get()
             sys.stdout.write("done\n")
             results.update(result)
-            p.join()
+            process.join()
             print('\t[*] %s#%.2d complete (%d processess discovered)' % (
                 'CPU' if not force_process_count else 'process',
                 cpu,
                 len(result)))
-        print
+        print()
         end_time = int(time.time())
         self._run_time = end_time - start_time
         return results
